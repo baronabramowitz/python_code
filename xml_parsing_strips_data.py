926a7d7150745dfd94c7a78e573e16b99b1d3926
Baron Abramowitz <baronabramowitz@users.noreply.github.com>
2016-08-19 13:29:36 +0100

    Cleaned file and clarified input parameters

--- xml_parsing_strips_data.py
+++ xml_parsing_strips_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def test_xml():
 	'''
-	This function takes an xml format as text from http://www.dmo.gov.uk/xmlData.aspx?rptCode=D3B.2&page=Gilts/Daily_Prices
+	This function takes an xml format (also works with plain text) from http://www.dmo.gov.uk/xmlData.aspx?rptCode=D3B.2&page=Gilts/Daily_Prices
 	and converts it into an excel spreadsheet of spot rates of strips for each strip maturity date. 
 	These strips can then be used to generate a yield curve with which different securities can be priced.
 	The use of xlwings t move the data into excel was just an exercise to create a more human readable format 
@@ -15,7 +15,7 @@ def test_xml():
 	pattern_g1_matches = []
 	pattern_g2_matches = []
 	match_list = []
-	for i, line in enumerate(open('/Users/baronabramowitz/Desktop/xml_as_plain_text')): #Feed XML output as text here.
+	for i, line in enumerate(open('/Users/baronabramowitz/Desktop/<SILO_DAILY_PRICES ISIN_CODE="GB0000514405" INSTRU.xml')): #Feed XML output as text here.
 	    for match in re.finditer(pattern, line):
 	    	match_list.append(match)
 	    	pattern_g1_matches.append(match.group(1)[:-1])
@@ -27,19 +27,11 @@ def test_xml():
 	pattern_g1_matches_df = pd.DataFrame(pattern_g1_matches)
 	pattern_g2_matches_df = pd.DataFrame(pattern_g2_matches)
 
-			#Input any Excel output file you'd like, but it makes most sense to put it on a new sheet
-	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet1').range('A1').value = pattern_g1_matches_df
-	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet1').range('A1').options(pd.DataFrame, expand='table').value
-	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet1').range('C1').value = pattern_g2_matches_df
-	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet1').range('C1').options(pd.DataFrame, expand='table').value
+		#Input any Excel output file you'd like, but it makes most sense to put it on a new sheet
+	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet2').range('A1').value = pattern_g1_matches_df
+	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet2').range('A1').options(pd.DataFrame, expand='table').value
+	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet2').range('C1').value = pattern_g2_matches_df
+	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet2').range('C1').options(pd.DataFrame, expand='table').value
 
 if __name__ == "__main__":
 	test_xml()
-
-
-
-
-
-'''from xml.etree import ElementTree as ET
-for line in 
-ET.fromstring(line).text'''
