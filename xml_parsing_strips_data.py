ee883894f85e17dcba0624f9fa96b971c2056409
Baron Abramowitz <baronabramowitz@users.noreply.github.com>
2016-08-19 18:30:00 +0100

    Added Functionality to Pull down XML
    Added scripts to pull down the XML and write it to a file, then used the file as the input for the previous function.
    The get request takes a while but the script would only need to be run once a day to retrieve the previous day's values.

--- xml_parsing_strips_data.py
+++ xml_parsing_strips_data.py
@@ -1,22 +1,34 @@
 import re
 import xlwings as xw
 import pandas as pd
+import requests
+#import webbrowser
 
 def test_xml():
 	'''
-	This function takes an XML format (also works with plain text) from http://www.dmo.gov.uk/xmlData.aspx?rptCode=D3B.2&page=Gilts/Daily_Prices
+	This function takes an xml format (also works with plain text) from http://www.dmo.gov.uk/xmlData.aspx?rptCode=D3B.2&page=Gilts/Daily_Prices
 	and converts it into an excel spreadsheet of spot rates of strips for each strip maturity date. 
 	These strips can then be used to generate a yield curve with which different securities can be priced.
 	The use of xlwings t move the data into excel was just an exercise to create a more human readable format 
 	without going to the horrors of VBA
-#"INSTRUMENT_NAME=\"Treasury Coupon Strip \d{2}[a-zA-Z]{3}2\d{3}\" REDEMPTION_DATE="(2\d{3}-\d{2}-\d{2})T.{157}YIELD=\"(\d{1,2}\.\d{12}\")
-#"INSTRUMENT_NAME=\"Treasury Coupon Strip \d{2}[a-zA-Z]{3}2\d{3}\" REDEMPTION_DATE="(2\d{3}-\d{2}-\d{2})T.{157}YIELD=\"(\d{1,2}\.\d{12}\")"
 	'''
+	#webbrowser.open('https://www.youtube.com/watch?v=UssqJLx0Er8', new = 1)
+	page = requests.get('http://www.dmo.gov.uk/xmlData.aspx?rptCode=D3B.2&page=Gilts/Daily_Prices')
+	if page.status_code==200:
+		raw_xml = open('/Users/baronabramowitz/Desktop/todays_strips_data_raw','w')
+		raw_xml.close()
+	else:
+		print("link invalid")
+	
+	#raw_xml = html.fromstring(page.content)
 	pattern = re.compile("INSTRUMENT_NAME=\"Treasury Coupon Strip \d{2}[a-zA-Z]{3}2\d{3}\" REDEMPTION_DATE=\"(2\d{3}[-][0-1][0-9][-][0-3][0-9])T.{157}YIELD=\"(\d{1,2}\.\d{12}\")")
 	pattern_g1_matches = []
 	pattern_g2_matches = []
 	match_list = []
-	for i, line in enumerate(open('/Users/baronabramowitz/Desktop/<SILO_DAILY_PRICES ISIN_CODE="GB0000514405" INSTRU.xml')): #Feed XML output as text here.
+	for i, line in enumerate(open('/Users/baronabramowitz/Desktop/todays_strips_data_raw')): 
+	#page.text):
+	#open('/Users/baronabramowitz/Desktop/<SILO_DAILY_PRICES ISIN_CODE="GB0000514405" INSTRU.xml')): 
+	#Feed XML output as text here.
 	    for match in re.finditer(pattern, line):
 	    	match_list.append(match)
 	    	pattern_g1_matches.append(match.group(1))
@@ -25,6 +37,7 @@ def test_xml():
 	#print (match_list)
 	#print (pattern_g1_matches)
 	#print (pattern_g2_matches)
+	#close('/Users/baronabramowitz/Desktop/todays_strips_data_raw')
 	pattern_g1_matches_df = pd.DataFrame(pattern_g1_matches)
 	pattern_g2_matches_df = pd.DataFrame(pattern_g2_matches)
 	strips_output = pd.merge(pattern_g1_matches_df,pattern_g2_matches_df, left_index=True, right_index = True)
@@ -32,7 +45,7 @@ def test_xml():
 	strips_output['Date'] = pd.to_datetime(strips_output['Date'])
 	strips_output = strips_output.sort_values('Date')
 	strips_output.index = range(0,len(strips_output))
-	#print(strips_output)
+	print(strips_output)
 
 			#Input any Excel output file you'd like, but it makes most sense to put it on a new sheet
 	xw.Book(r'/Users/baronabramowitz/Desktop/xlwings_testing_doc.xlsx').sheets('Sheet3').range('A1').value = strips_output
