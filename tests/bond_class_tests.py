4307220e23e3121ca16bdc3accece79bd511d068
Baron Abramowitz <baron.abramowitz@yahoo.com>
2016-10-06 20:54:43 +0100

    Created tests for bond and portfolio classes

---
+++ tests/bond_class_tests.py
@@ -0,0 +1,85 @@
+__author__ = 'Baron Abramowitz'
+__version__ = '0.1.0'
+__maintainer__ = 'Baron Abramowitz'
+__email__ = 'baron.abramowitz@yahoo.com'
+__date__ = '22/08/2016'
+
+import unittest
+import sys
+sys.path.append('/Users/baronabramowitz/Desktop/python_code/bond_functions')
+from bond_class import Bond
+
+class TestSuiteBondCode(unittest.TestCase):
+    """Large Selection of Tests for the bond_class.py
+
+    I have recently realized that these tests will need to point to fixed data sets for yields 
+    as the bond values will change day to day as the yield curves on which they are based move as well.
+    In the interim I've simply used ranges ov values to encompas possibilities for changes in the daily yield curve
+    """
+
+    def test_bond_value(self):
+        self.assertTrue( 10000 <= Bond(face_value = 10000.0,
+                maturity_date = '2022-06-15',
+                coupon_rate = 2.5,
+                payments_per_year = 2,
+                rating = 'AAA',
+                btype = 'Corporate').value() <= 12000 )
+
+    def test_bond_value_c(self):
+        self.assertTrue( 10000 <= Bond(face_value = 10000.0,
+                maturity_date = '2022-06-15',
+                coupon_rate = 2.5,
+                payments_per_year = 2,
+                rating = 'AAA',
+                btype = 'Corporate').value_c() <= 12000 )
+
+    def test_bond_duration(self):
+        self.assertTrue( 4 <= Bond(face_value = 10000.0,
+                maturity_date = '2022-06-15',
+                coupon_rate = 2.5,
+                payments_per_year = 2,
+                rating = 'AAA',
+                btype = 'Corporate').duration() <= 6 )
+
+    def test_bond_duration_c(self):
+        self.assertTrue( 4 <= Bond(face_value = 10000.0,
+                maturity_date = '2022-06-15',
+                coupon_rate = 2.5,
+                payments_per_year = 2,
+                rating = 'AAA',
+                btype = 'Corporate').duration_c() <= 6 )
+
+    def test_modified_bond_duration(self):
+        self.assertTrue( 4 <= Bond(face_value = 10000.0,
+                maturity_date = '2022-06-15',
+                coupon_rate = 2.5,
+                payments_per_year = 2,
+                rating = 'AAA',
+                btype = 'Corporate').modified_duration() <= 6 )
+
+    def test_modified_bond_duration_c(self):
+        self.assertTrue( 4 <= Bond(face_value = 10000.0,
+                maturity_date = '2022-06-15',
+                coupon_rate = 2.5,
+                payments_per_year = 2,
+                rating = 'AAA',
+                btype = 'Corporate').modified_duration_c() <= 6 )
+
+    def test_bond_convexity(self):
+        self.assertTrue( 34 <= Bond(face_value = 10000.0,
+                maturity_date = '2022-06-15',
+                coupon_rate = 2.5,
+                payments_per_year = 2,
+                rating = 'AAA',
+                btype = 'Corporate').convexity() <= 36 )
+
+    def test_bond_convexity_c(self):
+        self.assertTrue( 34 <= Bond(face_value = 10000.0,
+                maturity_date = '2022-06-15',
+                coupon_rate = 2.5,
+                payments_per_year = 2,
+                rating = 'AAA',
+                btype = 'Corporate').convexity_c() <= 36 )
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/portfolio_class_tests.py
@@ -0,0 +1,36 @@
+__author__ = 'Baron Abramowitz'
+__version__ = '0.1.0'
+__maintainer__ = 'Baron Abramowitz'
+__email__ = 'baron.abramowitz@yahoo.com'
+__date__ = '22/08/2016'
+
+import unittest
+import sys
+sys.path.append('/Users/baronabramowitz/Desktop/python_code/bond_functions')
+
+from bond_class import Bond
+from portfolio_class import Portfolio, generate_portfolio
+
+
+class TestSuiteBondCode(unittest.TestCase):
+    """Large Selection of Tests for the bond_stuff_in_progress.py
+
+    I have recently realized that these tests will need to point to fixed data sets for yields 
+    as the bond values will change day to day as the yield curves on which they are based move as well.
+    In the interim I've simply used ranges ov values to encompas possibilities for changes in the daily yield curve
+    """
+    portfolio = Portfolio('/Users/baronabramowitz/Desktop/bond_portfolio_data.csv')
+
+    def test_portfolio_value(self):
+        self.assertTrue( 5500000 <= portfolio.value() <= 6500000 )
+
+    def test_portfolio_duration(self):
+        self.assertTrue( 10 <= portfolio.duration() <= 12 )
+
+    def test_portfolio_convexity(self):
+        self.assertTrue( 130 <= portfolio.convexity() <= 150 )
+
+
+if __name__ == "__main__":
+    portfolio = Portfolio('/Users/baronabramowitz/Desktop/bond_portfolio_data.csv')
+    unittest.main()
