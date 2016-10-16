0ed52feb2344bdbb3660947c74dea968d38d7c5b
Baron Abramowitz <baron.abramowitz@yahoo.com>
2016-10-16 15:53:15 +0100

    Removed hard coding of payment step
    Previously the payment step was hard coded to create semi annual coupon bonds out of every bond regardless of whether or not they were actually semi annual. This fix uncovered  a slew of other bugs; see specific commits for other files for more detailed info.

--- bond_functions/date_functions.py
+++ bond_functions/date_functions.py
@@ -1,3 +1,4 @@
+"""Contains the functions related to dates"""
 __author__ = 'Baron Abramowitz'
 __version__ = '0.1.0'
 __maintainer__ = 'Baron Abramowitz'
@@ -6,35 +7,35 @@ __date__ = '05/10/2016'
 
 import bankdate as BD
 import strips_data_config as sdc
-from datetime import timedelta, datetime
-
-
-"""Contains the functions related to dates"""
 
 
 def yields_for_payment_dates(mat_date, pay_step):
     """Generate list of discount rates for coupon payment dates"""
     # Currently hard coded to USD as currency
-    days_to_mat_new_dates = days_to_payment(mat_date, pay_step)
+    days_to_mat_dates = days_to_payment(mat_date, pay_step)
+
     spl = sdc.todays_strips_data_usd
     payment_date_approximate_yields = [
-        float(spl(days_to_mat)) for days_to_mat in days_to_mat_new_dates]
+        float(spl(days_to_mat)) for days_to_mat in days_to_mat_dates]
     return payment_date_approximate_yields
 
 
 def yields_for_payment_dates_var(mat_date, pay_step, scenario_spl):
     """Generate list of discount rates for coupon payment dates"""
     # Currently hard coded to USD as currency
-    days_to_mat_new_dates = days_to_payment(mat_date, pay_step)
+
+    days_to_mat_dates = days_to_payment(mat_date, pay_step)
     spl = scenario_spl
     payment_date_approximate_yields = [
-        float(spl(days_to_mat)) for days_to_mat in days_to_mat_new_dates]
+        float(spl(days_to_mat)) for days_to_mat in days_to_mat_dates]
     return payment_date_approximate_yields
 
 
 def days_to_payment(mat_date, pay_step):
-    '''Return a list of days until coupon dates'''
-    new_dates = BD.date_range(mat_date, step='6m')
+    """Return a list of days until coupon dates"""
+
+    new_dates = BD.date_range(mat_date, pay_step)
+
     days_to_payment = [BD.BankDate().num_of_days(date) for date in new_dates]
     return days_to_payment
 
