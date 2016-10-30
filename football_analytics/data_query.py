5a8d1f4f38bf3b5960585e9abb7703faf6e68e3e
Baron Abramowitz <baron.abramowitz@yahoo.com>
2016-10-30 15:54:54 +0000

    New file for Football Analytics
    Query data

---
+++ football_analytics/data_query.py
@@ -0,0 +1,70 @@
+
+import sqlite3
+import pandas as pd
+
+def retrieve_data_impact_sample(league):
+    """Retrieve the month sample for comparison to non WB periods"""
+    league_dict = {'England Premier League':1729,'France Ligue 1':4769,'Germany 1. Bundesliga':7809,
+                'Italy Serie A':10257,'Portugal Liga ZON Sagres':17647,'Scotland Premier League':19694,
+                    'Spain LIGA BBVA':21518}
+    #first date in league_wb is the WB start date for Dec,
+    #second date is the wb end date For Jan
+    #Since the actual dates vary year to year and are league specific,
+    #the data for the date cutoffs is a rough estimate
+    league_wb = {'England Premier League':(None,None),'France Ligue 1':('24','10'),'Germany 1. Bundesliga':('23','18'),
+                'Italy Serie A':('23','05'),'Portugal Liga ZON Sagres':('18','05'),'Scotland Premier League':('31','17'),
+                    'Spain LIGA BBVA':('23','05')}
+    league_id = league_dict[league]
+    dec_date = league_wb[league][0]
+    jan_date = league_wb[league][1]
+    conn = sqlite3.connect('/Users/baronabramowitz/Downloads/database.sqlite')
+    if league == 'England Premier League':
+        df = pd.read_sql_query("""SELECT date, home_team_goal, away_team_goal FROM Match 
+                        WHERE league_id = 1729 
+                        AND (strftime('%m', date) = '12' AND strftime('%d', date) > '20')
+                        OR (strftime('%m', date) = '01' AND strftime('%d', date) < '15' );""",conn)
+    else:
+        # Retrieve data for sepcific league for month following following winter break
+        df = pd.read_sql_query("""SELECT date, home_team_goal, away_team_goal FROM Match 
+                        WHERE league_id = {league_id_qy} 
+                        AND (strftime('%m', date) = '01' AND strftime('%d', date) > {jan_date})
+                        OR (strftime('%m', date) = '02' AND strftime('%d', date) < {feb_date} );""".format(
+                            league_id_qy = league_id, jan_date = jan_date, feb_date = jan_date)
+                        ,conn)
+    conn.close()
+    return df
+
+def retrieve_data_outside_impact(league):
+    league_dict = {'England Premier League':1729,'France Ligue 1':4769,'Germany 1. Bundesliga':7809,
+                'Italy Serie A':10257,'Portugal Liga ZON Sagres':17647,'Scotland Premier League':19694,
+                    'Spain LIGA BBVA':21518}
+    #first date in league_wb is the WB start date for Dec,
+    #second date is the wb end date For Jan
+    #Since the actual dates vary year to year and are league specific,
+    #the data for the date cutoffs is a rough estimate
+    league_wb = {'England Premier League':(None,None),'France Ligue 1':('24','10'),'Germany 1. Bundesliga':('23','18'),
+                'Italy Serie A':('23','05'),'Portugal Liga ZON Sagres':('18','05'),'Scotland Premier League':('31','17'),
+                    'Spain LIGA BBVA':('23','05')}
+    league_id = league_dict[league]
+    dec_date = league_wb[league][0]
+    jan_date = league_wb[league][1]
+    conn = sqlite3.connect('/Users/baronabramowitz/Downloads/database.sqlite')
+    if league == 'England Premier League':
+        df = pd.read_sql_query("""SELECT date, home_team_goal, away_team_goal FROM Match 
+                        WHERE league_id = 1729 
+                        AND NOT (strftime('%m', date) = '12' AND strftime('%d', date) > '20')
+                        OR NOT (strftime('%m', date) = '01' AND strftime('%d', date) < '15' );""",conn)
+    else:
+        # Retrieve data for sepcific league for month following following winter break
+        df = pd.read_sql_query("""SELECT date, home_team_goal, away_team_goal FROM Match 
+                        WHERE league_id = {league_id_qy} 
+                        AND NOT (strftime('%m', date) = '01' AND strftime('%d', date) > {jan_date})
+                        OR NOT (strftime('%m', date) = '02' AND strftime('%d', date) < {feb_date} );""".format(
+                            league_id_qy = league_id, jan_date = jan_date, feb_date = jan_date)
+                        ,conn)
+    conn.close()
+    return df
+
+if __name__ == "__main__":
+    print(retrieve_data_outside_impact('Italy Serie A'))
+    print(retrieve_data_impact_sample('Italy Serie A'))
